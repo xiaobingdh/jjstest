98100f92fb7b79bbc607e5681ce4cde6a720c601
Lenovo <Lenovo@USER-20160405LC>
2017-01-13 17:00:17 +0800

    test

---
+++ src/main/java/com/zkr/jjs/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.zkr.jjs;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("=============");
+		req.setAttribute("username", "jjs");
+		req.getRequestDispatcher("/hellogit.jsp").forward(req, resp);
+	}
+}	
