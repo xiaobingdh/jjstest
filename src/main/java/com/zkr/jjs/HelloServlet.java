08f4c61030e1064fe4eca97c29489cb35888edcf
Lenovo <Lenovo@USER-20160405LC>
2017-01-16 10:23:20 +0800

    blackcat提交

--- src/main/java/com/zkr/jjs/HelloServlet.java
+++ src/main/java/com/zkr/jjs/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("=============");
+		System.out.println("======1111=======");
 		req.setAttribute("username", "jjs");
 		req.getRequestDispatcher("/hellogit.jsp").forward(req, resp);
 	}
